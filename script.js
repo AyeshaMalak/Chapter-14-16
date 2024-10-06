f23bf3ff6ff71a3cc2857a2a3fa809ff8efb40df
AyeshaMalak <ayeshamalak34@gmail.com>
2024-10-06 06:18:37 +0500

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-sortedScores.sort((a, b) => a - b);
-        console.log(sortedScores);
+scores.sort((a, b) => a - b);
+        console.log(scores);
 
         // 11. Copy elements from cities array to selectedCities array
-        let cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
-        let selectedCities = cities.slice(1, 4); // Copy from index 1 to 3
-        console.log(selectedCities);
+        let citiesNames = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+        let selectedCitiesNames = citiesNames.slice(1, 4); // Copy from index 1 to 3
+        console.log(selectedCitiesNames);
 
         // 12. Create a single string from the array using join method
-        let arr = ["This", " is", " my", " cat"];
-        let sentence = arr.join("");
+        let array = ["This", " is", " my", " cat"];
+        let sentence = array.join("");
         console.log(sentence);
 
         // 13. Reverse an array (Last In First Out)
@@ -23,10 +23,10 @@ sortedScores.sort((a, b) => a - b);
         }
 
         // 14. Store phone manufacturers and display a dropdown menu
-        let manufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+        let phoneManufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
         document.write("<h2>Phone Manufacturers:</h2>");
         document.write("<select>");
-        for (let i = 0; i < manufacturers.length; i++) {
-            document.write(`<option>${manufacturers[i]}</option>`);
+        for (let i = 0; i < phoneManufacturers.length; i++) {
+            document.write(`<option>${phoneManufacturers[i]}</option>`);
         }
         document.write("</select>");
